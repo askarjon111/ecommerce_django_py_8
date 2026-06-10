19045bbc2c65b01b71d83f4daf022f6cf5f9cb81
ASKAR <askarjon.abdullayev@gmail.com>
2026-06-10 20:02:14 +0500

    order detail

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from apps.orders.api_endpoints.orders.OrderCreate.views import order_create_view
+from apps.orders.api_endpoints.orders.OrderDetail.views import order_detail_view
 from apps.orders.api_endpoints.orders.OrderList.views import order_list_view
 
 
 urlpatterns = [
     path('', order_list_view, name='order-list'),
     path('create/', order_create_view, name='order-create'),
+    path('<int:pk>/', order_detail_view, name='order-detail'),
 ]
