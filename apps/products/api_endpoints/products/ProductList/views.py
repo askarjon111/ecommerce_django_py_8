baece3f05a1b2d97c90ff40ba5b61f511aef16da
ASKAR <askarjon.abdullayev@gmail.com>
2026-06-22 19:26:25 +0500

    pagination

--- apps/products/api_endpoints/products/ProductList/views.py
+++ apps/products/api_endpoints/products/ProductList/views.py
@@ -1,12 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.generics import ListAPIView
 
 from apps.products.api_endpoints.products.ProductList.serializers import ProductListSerializer
 from apps.products.models import Product
+from apps.products.pagination import CustomLimitOffsetPagination
 
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductListSerializer(products, many=True)
-    return Response(serializer.data)
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductListSerializer
+    pagination_class = CustomLimitOffsetPagination
--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from apps.products.api_endpoints.products.ProductList.views import product_list
+from apps.products.api_endpoints.products.ProductList.views import ProductListView
 from apps.products.api_endpoints.products.ProductDetail.views import product_detail
 from apps.products.api_endpoints.products.ProductCreate.views import product_create
 from apps.products.api_endpoints.products.ProductUpdateDestroy.views import product_update,product_destroy
 
 
 urlpatterns = [
-    path('', product_list, name='product_list'),
+    path('', ProductListView.as_view(), name='product_list'),
     path('<int:pk>/', product_detail, name='product_detail'),
     path('create/', product_create, name='product_create'),
     path('<int:pk>/update/', product_update, name='product_update'),
